bc7c3a66ecded3b2171b4ede3820dfe1225f3c60
Michael Ebel <mebel@christ-es.de>
2021-09-15 14:36:21 +0200

    main.cpp: Initialize the Webengine before constructing the GuiApplication
    This prevents a deprecation warning printed to the journal.
    
    Signed-off-by: Michael Ebel <mebel@christ-es.de>

--- main.cpp
+++ main.cpp
@@ -53,8 +53,8 @@ int main(int argc, char *argv[])
 {
     qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
 
-    QGuiApplication app(argc, argv);
     QtWebEngine::initialize();
+    QGuiApplication app(argc, argv);
     ApplicationEngine appEngine;
 
 
